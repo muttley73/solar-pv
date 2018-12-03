72a1ffcb714d1115a2981a397c1a8f98f485b65a
muttley <mattiello@gmail.com>
2018-12-03 16:50:08 +0100

    refactor

--- solmax/start.php
+++ solmax/start.php
@@ -5,15 +5,19 @@
  * Date: 03/12/18
  * Time: 15.03
  */
-include ('config.php');
-include ('SolarMax.php');
+include('config.php');
+include('SolarMax.php');
 
 $ADDR = "10.0.0.123";
 $PORT = "12345";
 $DEVICE_ADDR = "5";
 $TIMEOUT = 5; # seconds
 
-$sm = new SolarMax($ADDR,$PORT,$DEVICE_ADDR,$TIMEOUT);
-$c = $sm->getMessage('PAC');
+$sm = new SolarMax($ADDR, $PORT, $DEVICE_ADDR, $TIMEOUT);
+$c = [
+    $sm->getMessage('PAC'),
+    $sm->getMessage('KMT'),
+];
+
 
 var_dump($c);
