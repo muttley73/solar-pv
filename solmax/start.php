5eee7247624a055f539f6fc92512a47c3f986044
Mattiello Alessandro <mattiello@it.tiscali.com>
2018-12-06 11:35:55 +0100

    implementazione feeds

--- solmax/start.php
+++ solmax/start.php
@@ -9,8 +9,8 @@
 include('SolarMax.php');
 
 // *** config ***
-$lat = 39.1501857; #Latitude (+ to N)
-$lng = 8.9848062; #Longitude (+ to E)
+#$lat = 39.1501857; #Latitude (+ to N)
+#$lng = 8.9848062; #Longitude (+ to E)
 
 $ADDR = "10.0.0.123";
 $PORT = "12345";
@@ -21,18 +21,30 @@ $TIMEOUT = 5; # seconds
 
 $sunrise = new DateTime();
 $sunrise->setTimestamp(date_sunrise(time(), SUNFUNCS_RET_TIMESTAMP, $lat, $lng, 95));
-echo "alba: " . $sunrise->format("H:m:s") . "\n";
+#echo "alba: " . $sunrise->format("H:m:s") . "\n";
 
 $sunset = new DateTime();
 $sunset->setTimestamp(date_sunset(time(), SUNFUNCS_RET_TIMESTAMP, $lat, $lng, 95));
-echo "tramonto: " . $sunset->format("H:m:s") . "\n";
+#echo "tramonto: " . $sunset->format("H:m:s") . "\n";
 
 
 
 $sm = new SolarMax($ADDR, $PORT, $DEVICE_ADDR, $TIMEOUT);
 
 $f = $sm->generateReport();
+print_r($f);
+//echo $f['PAC']['value'];
 
-echo $f['PAC']['value'];
+if (!empty($f)){
+  $feed['status'] = 'on line';
+  foreach ($f as $key => $value){
+	$feed[$value['description']]=$value['value'];
+  }
 
-//print_r(json_encode($sm->generateReport()));
+}
+
+file_put_contents('feeds.json', json_encode($feed));
+
+print_r($feed);
+
+
