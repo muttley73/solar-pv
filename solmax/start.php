d7dfefced928bbf25dfa11d69d6dd16ba346849f
muttley <mattiello@gmail.com>
2018-12-11 11:45:44 +0100

    monolog config

--- solmax/start.php
+++ solmax/start.php
@@ -7,6 +7,11 @@
  */
 
 include('SolarMax.php');
+include('vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 
 // *** config ***
 $lat = 39.1501857; #Latitude (+ to N)
@@ -15,21 +20,34 @@ $lng = 8.9848062; #Longitude (+ to E)
 $ADDR = "10.0.0.123";
 $PORT = "12345";
 $DEVICE_ADDR = "5";
-$TIMEOUT = 2; # seconds
+$TIMEOUT = 5; # seconds
 $outputJsonFeeds = '/home/muttley/solar-pv/solmax/feeds.json';
 $pidFile = '/home/muttley/solar-pv/solmax/solarMax.pid';
+$logFile = "/home/muttley/solar-pv/solmax/log/" . date('Ymd', time()) . ".log";
+
+$log = new Logger('default');
+$log->pushHandler(new StreamHandler($logFile, Logger::DEBUG));
+
+
 if (file_exists($pidFile)) {
-    echo "\nprocesso già in esecuzione\n\n";
-    die();
+    if ($argv[1] == '-f') {
+        $log->debug("force mode, remove old pid file");
+        unlink($pidFile);
+    } else {
+        $log->error("found a pid file, another process is running");
+        die();
+    }
+
 }
 
 if (!file_put_contents($pidFile, '1')) {
-    echo "\nnon riesco a creare il file $pidFile\n\n";
+    $log->error("unable create pid file " . $pidFile);
     die();
 }
 
 function checkSun($lat, $lng) {
-
+    $sun = [];
+    $sun['position'] = 'above_horizon';
     $sunrise = new DateTime();
     $sunrise->setTimestamp(date_sunrise(time(), SUNFUNCS_RET_TIMESTAMP, $lat, $lng, 90));
     //echo "\n\nalba: ...............: " . $sunrise->format("H:i:s");
@@ -39,51 +57,67 @@ function checkSun($lat, $lng) {
     //echo "\ntramonto alle .......: " . $sunset->format("H:i:s");
 
     $now = new DateTime();
-    if ($now->getTimestamp() > $sunrise->getTimestamp() && $now->getTimestamp() < $sunset->getTimestamp()) {
-        //$timeToSunSet = $sunset->getTimestamp() - $now->getTimestamp();
-        //echo "\ntramonto fra $timeToSunSet secondi\n";
-        return 'on line';
-    } else {
-        return 'off line';
+    if ($sunset->getTimestamp() < $now->getTimestamp()){
+        // add 1 day to sunset
+        $sun['position'] = 'under_horizon';
+        $sunset->add(new DateInterval('P1D'));
     }
+    if ($sunrise->getTimestamp() < $now->getTimestamp()){
+        // add 1 day to sunset
+        $sunrise->add(new DateInterval('P1D'));
+    }
+
+    $timeToSunSet = $sunset->getTimestamp() - $now->getTimestamp();
+    $sun['sun_set'] = $sunset;
+    $sun['sun_set_second'] = $timeToSunSet;
+    $sun['set_human_date'] = $sunset->format(DATE_RFC3339);
+
+    $timeToSunRise = $sunrise->getTimestamp() - $now->getTimestamp();
+    $sun['sun_rise'] = $sunrise;
+    $sun['sun_rise_second'] = $timeToSunRise;
+    $sun['rise_human_date'] = $sunrise->format(DATE_RFC3339);
+
+    return $sun;
+
 }
+
 
 while (true) {
     try {
-        echo "start...";
-        $status = checkSun($lat, $lng);
-        echo "\n$status";
-        $log = DateTime::RFC822 . "|";
-        echo $log;
+
+        $log->debug(' ---> check sunrise and sunset time');
+        $sun = checkSun($lat, $lng);
+
+        if (!is_array($sun) || empty($sun)) {
+            $log->error('check sun error');
+        }
+        $log->debug($sun['position']);
+        $log->debug("sun rise at " . $sun['rise_human_date']. " - (".$sun['sun_rise_second'].") second(s)");
+        $log->debug("sun set at " . $sun['set_human_date']. " - (".$sun['sun_set_second'].") second(s)");
+
+
         $f = [];
-        if ($status == 'on line') {
+        if ($sun['position'] == 'above_horizon') {
+            $status = 'on line';
             $sm = new SolarMax($ADDR, $PORT, $DEVICE_ADDR, $TIMEOUT);
-            $log .= "connecting=";
-            echo $log;
+            $log->debug("connecting ... ");
             if ($sm->connect()) {
-                $log .= "[ok]|retrieve data=";
-                echo $log;
+                $log->debug("connect");
+                $log->debug("retrieve data");
                 $f = $sm->generateReport();
-                $log .= empty($f) ? "[failure]|" : "[ok]|";
-                $log .= "close connection=";
+                $log->debug("close connection");
                 $sm->close_connect();
-                $log .= "[ok]";
-                echo $log;
             } else {
                 file_put_contents($outputJsonFeeds, json_encode(array_merge(getFeedsArray($outputJsonFeeds), resetSensor())));
-                $log .= "write reset sensor|";
-                die();
+                $log->error("system off line lan error");
             }
-
-            $log .= "write feeds=";
-            echo "\nScrivo feeds";
 
         } else {
 
-	    file_put_contents($outputJsonFeeds, json_encode(array_merge(getFeedsArray($outputJsonFeeds), resetSensor())));
-            $log .= "write reset sensor|";
+            file_put_contents($outputJsonFeeds, json_encode(array_merge(getFeedsArray($outputJsonFeeds), resetSensor())));
+            $log->info("system off line sun above horizont");
 
-	}
+        }
 
         if (file_exists($outputJsonFeeds)) {
             $arrayFeeds = getFeedsArray($outputJsonFeeds);
@@ -97,18 +131,22 @@ while (true) {
             $arrayFeeds[$item['description']] = $item['value'];
         }
 
-        $log .= file_put_contents($outputJsonFeeds, json_encode($arrayFeeds)) ? "[ok]" : "[failure]";
+        if (!file_put_contents($outputJsonFeeds, json_encode($arrayFeeds))){
+            $log->error("write feed error ".$outputJsonFeeds);
+        }
 
-        echo $log . "\n";
         sleep(5);
     } catch (Exception $e) {
-        echo $e->getMessage();
+        $log->error($e->getMessage());
         die();
     }
 
 }
 
-echo "\n\n";
+$log->debug("exit ...");
+$log->close();
+
+
 die();
 
 // ****** function *******
